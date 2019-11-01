4050c721aad9b396ca059b751b502e3c19ff6ff6
robi.panczel <robi.panczel@gmail.com>
2019-11-01 12:51:49 +0100

    Accepted assigment

--- Freivalds.cpp
+++ Freivalds.cpp
@@ -12,9 +12,9 @@
 
 void vectorToConsole(std::vector<int> vector) {
     for (auto el : vector) {
-                // std::cout << el << ' ';
+                std::cout << el << ' ';
             }
-            // std::cout << "\n";
+            std::cout << "\n";
 }
 
 int randomNumber(int min, int max) {
@@ -25,8 +25,6 @@ int randomNumber(int min, int max) {
 
 // Multiplies a matrix with a vector, and returns the vector result
 std::vector<int> matrixVectorMultiplier(std::vector<std::vector<int>> matrix, std::vector<int> vector) {
-    // std::this_thread::sleep_for(std::chrono::seconds(3)); // DELETE THIS!!!!!
-    // Calc matrix * vector and return it
     std::vector<int> tempVector;
     for (int i = 0; i < vector.size(); i++)
     {
@@ -38,41 +36,21 @@ std::vector<int> matrixVectorMultiplier(std::vector<std::vector<int>> matrix, st
         tempVector.push_back(tempRowSum);
         
     }
-    
-    { // Write matrix * vector to console
-        // // std::cout << "The matrix: ";
-        // for (auto row : matrix) {
-        //     vectorToConsole(row);
-        //     // std::cout << "\n";
-        // }
-        // // std::cout << "Random vector: ";
-        // vectorToConsole(vector);
-        // // std::cout << "CalcVector: ";
-        // vectorToConsole(tempVector);
-        // // std::cout << "___" << std::endl;
-    }
 
     return tempVector;
 }
 
-// Using the Freivalds method check if (a + b = c) is true
+// Using the Freivalds method check if (a * b = c) is true
 int checkMatrix(std::vector<std::vector<int>> aMatrix, std::vector<std::vector<int>> bMatrix, std::vector<std::vector<int>> cMatrix) {
-    // std::this_thread::sleep_for(std::chrono::seconds(3)); // DELETE THIS!!!!!
     
     std::vector<int> alpaV; //random {0,1} vector
     for (int i = 0; i < aMatrix.size(); i++) {
         int tempR = randomNumber(0,1);
         alpaV.push_back(tempR);
     }
-    // std::cout << "ALPAV: \n" << std::endl;
-    vectorToConsole(alpaV);
     std::vector<int> betaV = matrixVectorMultiplier( aMatrix, matrixVectorMultiplier(bMatrix, alpaV));
     std::vector<int> gammaV = matrixVectorMultiplier(cMatrix, alpaV);
-    if (betaV == gammaV) {
-        return 0;
-    } else {
-        return 1;
-    }
+    return betaV == gammaV;
 }
 
 int main() {
@@ -91,17 +69,18 @@ int main() {
         std::istringstream iss(tempstr);
         iss >> vectorDim;
         iss >> tryCount;
-        // std::cout << "dim: " << vectorDim << " try: " << tryCount << std::endl;
     }
+
+    // Separating the matrixes
     while (std::getline(ifs, tempstr)) {
         std::istringstream iss(tempstr);
         std::vector<int> tempv;
         while (iss >> tempint) {
             tempv.push_back(tempint);
         }
-        if (lineCounter < 3) {
+        if (lineCounter < vectorDim) {
             aMatrix.push_back(tempv);
-        } else if (lineCounter < 6) {
+        } else if (lineCounter < 2*vectorDim) {
             bMatrix.push_back(tempv);
         } else {
             cMatrix.push_back(tempv);
@@ -115,19 +94,17 @@ int main() {
         futureResults.push_back(std::async(std::launch::async, checkMatrix, aMatrix, bMatrix, cMatrix));
     }
 
-    int result = 0;
-    // Get the result from the threads
+    int result = 1;
+    // Getting the result from the threads
     for (int i = 0; i < tryCount; i++) {
-        result = result + futureResults[i].get();
+        result *= futureResults[i].get();
     }
 
     std::ofstream out("output.txt");
-    if (result == 0) {
+    if (result == 1) {
         out << "accepted";
-        // // std::cout << "accepted" << std::endl;
     } else {
         out << "rejected";
-        // // std::cout << "rejected" << std::endl;
     }
     out.close();
 }
